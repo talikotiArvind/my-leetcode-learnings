34d9fee11236148f3087d7fde39d11fb90d1a1fc
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-14 23:32:31 +0530

    Implement binary search function in py_code.py

---
+++ Binary Search/py_code.py
@@ -0,0 +1,11 @@
+def binary_search(nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
