b533f13af98fc38f1c5cb539873f019fb0d45989
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-16 23:50:33 +0530

    Fix output initialization in reversed_excel_sheet_column_table
    Initialize output variable to None before use.

--- Excel Sheet Column Title/reversed_py_code.py
+++ Excel Sheet Column Title/reversed_py_code.py
@@ -1,4 +1,5 @@
 def reversed_excel_sheet_column_table(columnNumber: str):
+  output = None
   mapper = {chr(i + 64): i for i in range(1,27)}
   for index, element in enumerate(columnNumber[::-1]):
       value = (26 ** index) * (mapper[element])
