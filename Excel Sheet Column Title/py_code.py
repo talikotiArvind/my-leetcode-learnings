51be1f7c19272139f07a062e25bfc8c7e10bf6bd
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-16 23:42:31 +0530

    Add function to convert column number to title

---
+++ Excel Sheet Column Title/py_code.py
@@ -0,0 +1,7 @@
+def convertToTitle(columnNumber: int):
+    result = []
+    while columnNumber > 0:
+        columnNumber -= 1
+        result.append(chr(columnNumber % 26 + ord('A')))
+        columnNumber //= 26
+    return ''.join(reversed(result))
