66974498fd2b5b17afe87aabf6c75e68fd19ffea
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-25 21:47:00 +0530

    Implement intersection function for two arrays

---
+++ Intersection of 2 arrays II/py_code.py
@@ -0,0 +1,13 @@
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        count = Counter(nums1)
+        result = []
+
+        for n in nums2:
+            if count[n] > 0:
+                result.append(n)
+                count[n] -= 1
+
+        return result
