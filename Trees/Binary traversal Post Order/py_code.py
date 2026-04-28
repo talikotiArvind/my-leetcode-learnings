a394a468b58ad8219f50dfea4ae3774bcbf05813
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-28 22:35:39 +0530

    Create py_code.py

---
+++ Trees/Binary traversal Post Order/py_code.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        output = []
+        def traverse(node):
+            if not node:
+                return
+            traverse(node.left)
+            traverse(node.right)
+            output.append(node.val)
