b71f230b5bdbfdb3c0ee9ae24f0a310455e2c541
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-04-29 23:26:27 +0530

    Implement hasPathSum method in Solution class

---
+++ Trees/Path Sum/py_code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == targetSum
+        remaining = targetSum - root.val
+        return self.hasPathSum(root.left, remaining) or self.hasPathSum(root.right, remaining)
