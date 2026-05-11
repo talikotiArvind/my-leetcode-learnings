681721f09d028df46107e3f15fb5bda4c95711b1
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-05-11 22:01:50 +0530

    Implement function to find intersection of two linked lists

---
+++ Intersection of two linked lists/py_code.py
@@ -0,0 +1,6 @@
+def getIntersectionNode(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b = headA, headB
+        while a != b:
+            a = a.next if a else headB
+            b = b.next if b else headA
+        return a
