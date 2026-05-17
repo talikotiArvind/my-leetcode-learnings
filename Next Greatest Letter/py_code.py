8aa30dc41262263b9c04c9b00bea2ba57deb1a93
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-05-17 15:43:19 +0530

    Implement nextGreatestLetter function

---
+++ Next Greatest Letter/py_code.py
@@ -0,0 +1,10 @@
+def nextGreatestLetter(letters: List[str], target: str) -> str:
+        low, high = 0, len(letters)
+
+        while low < high:
+            mid = (low + high) // 2
+            if letters[mid] <= target:
+                low = mid + 1
+            else:
+                high = mid
+        return letters[low % len(letters)]
