949214510618578ce4daf63c6d0340c7c879098d
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-05-25 23:44:15 +0530

    Implement sumOfLeftLeaves function to calculate sum

---
+++ Trees/Sum of left leaves/py_code.py
@@ -0,0 +1,9 @@
+def sumOfLeftLeaves(root) -> int:
+    def dfs(node, is_left):
+        if not node:
+            return 0
+        if not node.left and not node.right:
+            return node.val if is_left else 0
+        return dfs(node.left, True) + dfs(node.right, False)
+
+    return dfs(root, False)
