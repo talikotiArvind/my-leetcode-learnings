a401fe6f80ef88a47326eefadba1fc20dc3572b5
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-06-04 19:31:48 +0530

    Implement function to count distinct substrings of size three

---
+++ Substrings of Size Three with Distinct Characters/py_code.py
@@ -0,0 +1,8 @@
+def countGoodSubstrings(s: str) -> int:
+        n = len(s)
+        count = 0
+
+        for i in range(n-2):
+            if s[i] != s[i+1] and s[i+1]!= s[i+2] and s[i+2] != s[i]:
+                count += 1
+        return count
