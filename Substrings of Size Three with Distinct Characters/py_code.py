0f682907e20033519291b56b4516a6723b550d7b
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-06-04 19:33:19 +0530

    Update return statement in py_code.py
    Fix return statement formatting for clarity.

--- Substrings of Size Three with Distinct Characters/py_code.py
+++ Substrings of Size Three with Distinct Characters/py_code.py
@@ -5,4 +5,4 @@ def countGoodSubstrings(s: str) -> int:
         for i in range(n-2):
             if s[i] != s[i+1] and s[i+1]!= s[i+2] and s[i+2] != s[i]:
                 count += 1
-        return count
+        return count #return count
