fec23c03b2104c64fee19f7e345cec37c7e2fbf9
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-06-24 23:29:11 +0530

    Implement minimum absolute difference function

---
+++ Minimum absolute difference/py_code.py
@@ -0,0 +1,6 @@
+def minimumAbsDifference(arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = min(arr[i + 1] - arr[i] for i in range(len(arr) - 1))
+        return [[arr[i], arr[i + 1]]
+                for i in range(len(arr) - 1)
+                if arr[i + 1] - arr[i] == min_diff]
