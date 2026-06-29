daea279bd5686fd70abc12271cde98c374e26346
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-06-29 22:41:43 +0530

    Implement minSubArrayLen function for subarray sum

---
+++ Minimum size subarray sum/py_code.py
@@ -0,0 +1,13 @@
+def minSubArrayLen(target: int, nums: List[int]) -> int:
+        left = 0
+        window_sum = 0
+        best = float('inf')
+
+        for right in range(len(nums)):
+            window_sum += nums[right]
+
+            while window_sum >= target:
+                best = min(best, right - left + 1)
+                window_sum -= nums[left]
+                left += 1
+        return best if best != float('inf') else 0
