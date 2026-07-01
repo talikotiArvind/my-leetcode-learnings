335cdf3b9e06751c1953a8dafcf08f6c41ba5ebd
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-07-01 22:49:21 +0530

    Add function to remove outer parentheses from string

---
+++ Remove Outermost paranthesis/py_code.py
@@ -0,0 +1,13 @@
+def removeOuterParentheses(s):
+    result = []
+    depth = 0
+    for ch in s:
+        if ch == "(":
+            if depth > 0:
+                result.append(ch)
+            depth += 1
+        else:  # ")"
+            depth -= 1
+            if depth > 0:
+                result.append(ch)
+    return "".join(result)
