a0ea560ed28717e52063662665371f3456509b0e
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-07-06 23:14:23 +0530

    Implement maxNumberOfBalloons function

---
+++ Maximum number of balloons/py_code.py
@@ -0,0 +1,9 @@
+def maxNumberOfBalloons(self, text: str) -> int:
+        count = Counter(text)
+        return min(
+            count['b'],
+            count['a'],
+            count['l'] // 2,
+            count['o'] // 2,
+            count['n'],
+        )
