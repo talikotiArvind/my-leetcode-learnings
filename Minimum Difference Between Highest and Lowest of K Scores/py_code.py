811f05793ebfdd7d3f374f1a6fea0a9d1fe23686
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-07-17 12:01:44 +0530

    Implement function to find minimum score difference

---
+++ Minimum Difference Between Highest and Lowest of K Scores/py_code.py
@@ -0,0 +1,5 @@
+def minimumDifference(nums: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+        nums.sort()
+        return min(nums[i+k-1] - nums[i] for i in range(len(nums) - k + 1))
