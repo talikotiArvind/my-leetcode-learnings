60d5f34d37a151e77eb2d576a89c4ed954a68473
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-07-26 01:23:00 +0530

    Add firstMissingPositive function
    Implement function to find the first missing positive integer in a list.

---
+++ Hard/First Missing Positive/py_code.py
@@ -0,0 +1,11 @@
+def firstMissingPositive(nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+                correct_index = nums[i] - 1
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n+1
