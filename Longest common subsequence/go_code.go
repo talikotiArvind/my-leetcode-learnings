7a6b48f0e447ce0273a3ffc52ab23b373f51239e
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2026-08-19 23:05:04 +0530

    Implement longest common subsequence algorithm

---
+++ Longest common subsequence/go_code.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	check := func(got, want int) {
+		if got != want {
+			panic(fmt.Sprintf("got %d, want %d", got, want))
+		}
+	}
+	check(longestCommonSubsequence("abcde", "ace"), 3)
+	check(longestCommonSubsequence("abc", "abc"), 3)
+	check(longestCommonSubsequence("abc", "def"), 0)
+	check(longestCommonSubsequence("", "abc"), 0)
+	check(longestCommonSubsequence("bsbininm", "jmjkbkjkv"), 1)
+	fmt.Println("All tests passed.")
+}
